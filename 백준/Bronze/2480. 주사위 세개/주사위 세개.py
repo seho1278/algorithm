61b38409318aec0df00280d1f04027c7db4c83fd
seho1278 <121420628+seho1278@users.noreply.github.com>
2023-01-16 15:06:39 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,22 +1,20 @@
-n1, n2, n3 = map(int, input().split()) 
-p = 0
+numbers = map(int, input().split())
+result = []
 
-if n1 == n2 and n2 == n3:
-    p = 10000 + n1 * 1000
-elif n1 == n2 or n2 == n3 or n1 == n3:
-    if n1 == n2:
-        p = 1000 + n1 * 100
-    elif n2 == n3:
-        p = 1000 + n2 * 100
+for n in numbers:
+    result.append(n)
+
+prize = 0
+prize_list = []
+
+for i in result:
+    if result.count(i) == 3:
+        prize_list.append(10000+(i*1000))
+        
+    elif result.count(i) == 2:
+        prize_list.append(1000+(i*100))
+        
     else:
-        p = 1000 + n3 * 100
+        prize_list.append(max(result)*100)
 
-elif n1 != n2 and n1 != n3 and n2 != n3:
-    if n1 > n2 and n1 > n3:
-        p = n1 * 100
-    elif n2 > n1 and n2 > n3:
-        p = n2 * 100
-    else:
-        p = n3 * 100
-
-print(p)
+print(max(prize_list))
