1204cd6c548a94b4cce431a1911782e82c2ed4f4
seho1278 <121420628+seho1278@users.noreply.github.com>
2023-01-18 16:44:13 +0900

    [Bronze I] Title: 나무 조각, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -0,0 +1,12 @@
+numbers = list(map(int, input().split()))
+r_numbers = sorted(numbers)
+
+while True:
+    if numbers != r_numbers:
+        for n in range(len(numbers)-1):
+            if numbers[n] > numbers[n+1]:      
+                numbers[n], numbers[n+1] = numbers[n+1], numbers[n]
+                print(*numbers)
+    
+    else:
+        break
