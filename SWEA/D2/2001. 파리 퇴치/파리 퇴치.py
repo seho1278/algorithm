e07af17c91e0e316b5633731f9353df4f768381b
seho1278 <121420628+seho1278@users.noreply.github.com>
2023-02-03 16:53:54 +0900

    [D2] Title: 파리 퇴치, Time: 163 ms, Memory: 61,736 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -2,19 +2,16 @@ T = int(input())
 
 for t in range(T):
     N, M = map(int, input().split())
-    M_list = []
-    N_list = []
+    fly = [list(map(int, input().split())) for _ in range(N)]
 
-    for n in range(N):
-        numbers = list(map(int, input().split()))
-        N_list.append(numbers)
+    result = []
 
-    for k in range(N-M+1):
-        for l in range(N-M+1):
-            R_list = []
-            for i in range(M):
-                for j in range(M):
-                    R_list.append(N_list[k + i][l + j])
-            M_list.append(sum(R_list))
+    for i in range(N-M+1):
+        for j in range(N-M+1):
+            cnt = 0
+            for k in range(j, j+M):
+                for l in range(i, i+M):
+                    cnt += fly[k][l]
 
-    print(f'#{t+1} {max(M_list)}')
+            result.append(cnt)
+    print(f'#{t+1} {max(result)}')
