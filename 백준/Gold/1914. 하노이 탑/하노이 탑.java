631328c80b8f52fb69504d49ce2fd2c5b2148ee2
seho1278 <121420628+seho1278@users.noreply.github.com>
2025-08-09 17:55:04 +0900

    [Gold V] Title: 하노이 탑, Time: 4016 ms, Memory: 128376 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.java
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.java
@@ -1,11 +1,29 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
+/**
+ * 하노이의 탑
+ * 
+ * 대표적인 재귀 구현 문제
+ * 
+ * 1 2 3 의 장대가 있을때
+ * 1에 있는 모든 원판을 3의 원판으로 옮기기
+ * 
+ * 1.
+ * 1에 있는 N 크기의 원판을 3으로 옮기기 위해선 N-1개의 원판을 2로 옮겨야함
+ * 2로 N-1개를 옮길 때 3을 보조바로 사용
+ * 
+ * 2.
+ * N -> 3이동
+ * 
+ * 3.
+ * 1 선반을 보조바로 이용해 N-1개 원판을 3으로 옮기기
+ * 재귀를 어떤 형태로 구현해야하나??
+ */
 public class Main {
 	public static void main(String[] args) {
 			Scanner sc = new Scanner(System.in);
 			int n = sc.nextInt();
-			// BigInteger를 사용한 이동횟수 출력
 			System.out.println(new BigInteger("2").pow(n).subtract(BigInteger.ONE));
 			if (n <= 20) {
 				hanoi(n, 1, 3, 2);
