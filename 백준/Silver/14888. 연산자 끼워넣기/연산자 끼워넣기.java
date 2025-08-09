6359da8426d000ca46344590f1f10c5fff3f0aad
seho1278 <121420628+seho1278@users.noreply.github.com>
2025-08-09 23:57:54 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 396 ms, Memory: 18024 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,83 +1,105 @@
-
-/*
- * silver1_14888 연산자 끼워넣기
- */
-
+import java.util.Arrays;
 import java.util.Scanner;
 
+/* 연산자 끼워넣기
+ * 문제 관련 내용은 기록 참고
+ */
+
 public class Main {
-	static int N;
+	// 배열들은 전역 변수로 관리
 	static int[] numbers;
-	static int[] operatorCnt = new int[4];
-	
-	// 최댓값과 최솟값
-	static int max = Integer.MIN_VALUE;
-	static int min = Integer.MAX_VALUE;
-	
-	// 연산
-	private static int calculate(int num1, int operatorIdx, int num2) {
-		int result = 0;
-		
-		switch (operatorIdx) {
-			case 0:
-				result = num1 + num2;
-				break;
-			case 1:
-				result = num1 - num2;
-				break;
-			case 2:
-				result = num1 * num2;
-				break;
-			case 3:
-				result = num1 / num2;
-				break;
-		}
-		
-		return result;
+	static char[] operators;
+	static boolean[] visited;
+	static int[] result = new int[2];
+	static {
+		result[0] = Integer.MIN_VALUE;
+		result[1] = Integer.MAX_VALUE;
 	}
 	
-	// 모든 경우의 수 탐색
-	public static void dfs(int result, int depth) {
-		if (depth == N - 1) {
-			max = Math.max(max, result);
-			min = Math.min(min, result);
+	// 계산기 (계산할 현재 값 current와 다음 숫자 next, 연산자를 파라미터 값으로 전달)
+	public static int calculator(int current, int next, char operator) {
+		// 연산자에 따라 계산 수행
+		switch (operator) { 
+			case '+':
+				return current + next;
+			case '-':
+				return current - next;
+			case '*':
+				return current * next;
+			case '/':
+				return current / next;
+		}
+		return -1; // 형식상 작성
+	}
+	
+	// dfs(순회 후 구한 최댓값과 최솟값을 배열에 저장해 반환)
+	// 현재 값과 깊이 depth, 최댓값과 최솟값을 저장할 배열를 파라미터 값으로 전달
+	public static void dfs(int current, int depth) {
+		// BaseCase
+		// depth가 numbers.length가 될경우 모든 수를 사용했으므로 최종 연산 결과를 반환한다.
+		if (depth == numbers.length) {
+			result[0] = Math.max(result[0], current);
+			result[1] = Math.min(result[1], current);
 			return;
 		}
 		
-		for (int i = 0; i < 4; i++) {
-			if (operatorCnt[i] > 0) {
-				operatorCnt[i]--;
+		// Recursive step
+		// 첫번째로 사용할 연산자 선택
+		for (int i = 0; i < operators.length; i++) {
+			// 해당 연산자가 사용중인지 확인
+			if (!visited[i]) {
+				// 해당 연산자 방문 처리
+				visited[i] = true;
 				
-				int nextResult = calculate(result, i, numbers[depth + 1]);
-				dfs(nextResult, depth + 1);
+				// 현재 값과 다음 값을 넣어 계산한 결과를 저장
+				int operate = calculator(current, numbers[depth], operators[i]);
 				
-				operatorCnt[i]++;
+				// 계산된 값과 depth를 1증가시켜 새로운 dfs 호출
+				dfs(operate, depth + 1);				
+				
+				// 사용한 연산자를 다시 원래 상태로 복구
+				visited[i] = false;
 			}
 		}
 	}
 	
-	
 	public static void main(String[] args) {
+		// 입력
 		Scanner sc = new Scanner(System.in);
 		
-		N = sc.nextInt();
+		int N = sc.nextInt();
 		
+		// N개 수를 저장할 배열 numbers 생성
 		numbers = new int[N];
 		
-		// 배열에 숫자 입력
+		// N개 배열에 피연산자 입력
 		for (int i = 0; i < numbers.length; i++) {
 			numbers[i] = sc.nextInt();
 		}
 		
-		// 연산자 입력
+		// 연산자를 저장할 배열 operators 생성
+		operators = new char[N-1];
+		// 연산자 수에 맞게 방문처리를 도와줄 배열 visited 생성
+		visited = new boolean[N-1];
+		
+		// 연산자를 풀어서 저장하기 위해 임시로 사용
+		char[] tmp = {'+', '-', '*', '/'};
+		int tmpIdx = 0; 
+		
+		// 주어지는 연산자들을 풀어서 저장
 		for (int i = 0; i < 4; i++) {
-			operatorCnt[i] = sc.nextInt();
+			// 해당 연산자 개수만큼 반복하며 저장
+			int t = sc.nextInt();
+			for (int j = 0; j < t; j++) {
+				operators[tmpIdx++] = tmp[i];
+			}
 		}
 		
-		dfs(numbers[0], 0);
+		// 순열을 돌릴 dfs 현재 값과 depth를 1로 설정하여 전달
+		dfs(numbers[0], 1);
 		
-		System.out.println(max);
-		System.out.println(min);
-		
+		// 최댓값과 최솟값 출력
+		System.out.println(result[0]);
+		System.out.println(result[1]);
 	}
 }
