32ae3a0c0d2a0c1a22b15adc14170d2d0d2afad9
seho1278 <121420628+seho1278@users.noreply.github.com>
2025-10-05 04:58:29 +0900

    [Gold III] Title: 택배, Time: 684 ms, Memory: 28100 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
@@ -12,31 +9,13 @@ import java.util.StringTokenizer;
  * 집하장에서 다른 집하장으로 최단경로로 화물을 이동시키기 위해 가장 먼저 거처야 하는 집하장이 있음
  * 4행 5열에 6이 있다면 4번에서 5번으로 최단 경로로 가기 위해서는 제일 먼저 6번 집하장으로 이동해야함
  * 
- * 이 때 경로표를 구하는 프로그램 작성
- * 그래프 생성 후
- * 각 정점에서 다익스트라 진행 후 첫번째 이동 정점 추출하기
+ * 모든 정점에서 모든 정점으로 이동하는 경우 플로이드 워셜 사용
  */
 public class Main {
-	
-	static class Node implements Comparable<Node> {
-		int v;
-		int w;
-		
-		public Node(int v, int w) {
-			this.v = v;
-			this.w = w;
-		}
-		
-		@Override
-		public int compareTo(Node o) {
-			return this.w - o.w;
-		}
-	}
-	
 	static int V, E;
-	static List<List<Node>> graph;
 	static final int INF = 987654321;
 	static int[][] matrix;
+	static int[][] result;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,12 +25,8 @@ public class Main {
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		
-		graph = new ArrayList<>();
 		matrix = new int[V + 1][V + 1];
-		
-		for (int i = 0; i <= V; i++) {
-			graph.add(new ArrayList<>());
-		}
+		result = new int[V + 1][V + 1];
 		
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -59,54 +34,52 @@ public class Main {
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			
-			graph.get(u).add(new Node(v, w));
-			graph.get(v).add(new Node(u, w));
+			matrix[u][v] = w;
+			matrix[v][u] = w;
+			result[u][v] = v;
+			result[v][u] = u;
 		}
 		
-		for (int i = 1; i <= V; i++) {
-			dijkstra(i);
-		}
+		floyd();
 		
+		// StringBuilder를 사용하면 시간을 더 줄일 수 있음
 		for (int i = 1; i <= V; i++) {
 			for (int j = 1; j <= V; j++) {
 				if (i == j) {
 					System.out.print("-" + " ");
 				} else {
-					System.out.print(matrix[i][j] + " ");					
+					System.out.print(result[i][j] + " ");					
 				}
 			}
 			System.out.println();
 		}
 	}
 	
-	public static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		int[] dist = new int[V + 1];
-		int[] first = matrix[start];
-		Arrays.fill(dist, INF);
+	public static void floyd() {
+		int[][] dp = new int[V + 1][V + 1];
 		
-		pq.offer(new Node(start, 0));
-		dist[start] = 0;
+		// dp 배열 초기화
+		for (int i = 1; i <= V; i++) {
+			Arrays.fill(dp[i], INF);
+		}
 		
-		while(!pq.isEmpty()) {
-			Node cur = pq.poll();
-			int u = cur.v;
-			int w = cur.w;
-			
-			if (dist[u] < w) {
-				continue;
+		for (int i = 1; i <= V; i++) {
+			for (int j = 1; j <= V; j++) {
+				if (i == j) {
+					dp[i][j] = 0;
+				} else if (matrix[i][j] != 0) {
+					dp[i][j] = matrix[i][j];
+				}
 			}
-			
-			for (Node node: graph.get(u)) {
-				int newW = w + node.w;
-				if (dist[node.v] > newW) {
-					dist[node.v] = newW;
-					if (u == start) {
-						first[node.v] = node.v; 
-					} else {
-						first[node.v] = first[u];
+		}
+		
+		for (int k = 1; k <= V; k++) {
+			for (int i = 1; i <= V; i++) {
+				for (int j = 1; j <= V; j++) {
+					if (dp[i][j] > dp[i][k] + dp[k][j]) {
+						dp[i][j] = dp[i][k] + dp[k][j];
+						result[i][j] = result[i][k];						
 					}
-					pq.offer(new Node(node.v, newW));
 				}
 			}
 		}
